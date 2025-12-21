c17c3fb4de987184803862bffff686edf73f3985
Rick Donalson <rdonalson@users.noreply.github.com>
2025-12-21 18:54:49 -0500

    Parent & First Child:  API Module Setup
    Parent & First Child:  API Module Setup

---
+++ API-JDK/module-api/src/test/java/com/Financial_Planner/module_api/ModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Financial_Planner.module_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
