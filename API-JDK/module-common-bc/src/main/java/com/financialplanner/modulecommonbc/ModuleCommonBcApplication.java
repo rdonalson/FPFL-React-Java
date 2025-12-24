b4b34187424793490d278149782f4624ec4cdf65
Rick Donalson <rdonalson@users.noreply.github.com>
2025-12-24 16:54:43 -0500

    JDK - Added Common Project
    JDK - Added Common Project

---
+++ API-JDK/module-common-bc/src/main/java/com/financialplanner/modulecommonbc/ModuleCommonBcApplication.java
@@ -0,0 +1,13 @@
+package com.financialplanner.modulecommonbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCommonBcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleCommonBcApplication.class, args);
+    }
+
+}
---
+++ API-JDK/module-common-bc/src/test/java/com/financialplanner/modulecommonbc/ModuleCommonBcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialplanner.modulecommonbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCommonBcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
