0dac3b357512a33b1e53b31be82855e5754b3cea
Rick Donalson <rdonalson@users.noreply.github.com>
2026-01-14 19:03:24 -0500

    * Removed Old Models in Domain

--- API-JDK/module-items-bc/src/main/java/com/financialplanner/moduleitemsbc/domain/repository/TimePeriodRepository.java
+++ API-JDK/module-items-bc/src/main/java/com/financialplanner/moduleitemsbc/domain/repository/TimePeriodRepository.java
@@ -10,16 +10,15 @@ import java.util.Optional;
  * Provides CRUD operations for persisting and retrieving time period-related data
  * from the underlying data source.
  *
- * This interface defines the contract for managing TimePeriod entities and transforms
- * them into domain models. It facilitates a separation between business logic
- * and persistence operations.
+ * This interface defines the contract for managing TimePeriod entities. It abstracts
+ * persistence details and facilitates interaction between the application and the data store.
  */
 public interface TimePeriodRepository {
-    com.financialplanner.moduleitemsbc.domain.model.TimePeriod save(TimePeriod entity);
+    TimePeriod save(TimePeriod entity);
 
-    Optional<com.financialplanner.moduleitemsbc.domain.model.TimePeriod> findById(Long id);
+    Optional<TimePeriod> findById(Long id);
 
-    List<com.financialplanner.moduleitemsbc.domain.model.TimePeriod> findAll();
+    List<TimePeriod> findAll();
 
     void deleteById(Long id);
 }
