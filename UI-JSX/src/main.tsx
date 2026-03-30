f4df7a73b0fe7e85ff8509526082a8837ada140a
Rick Donalson <rdonalson@users.noreply.github.com>
2026-03-30 18:43:45 -0400

    Reorg of files & styling back

--- UI-JSX/src/main.tsx
+++ UI-JSX/src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { AppProviders } from './app/providers/AppProviders.tsx';
 
-// PrimeReact global styles
-//import 'primereact/resources/themes/lara-light-purple/theme.css';
-//import 'primereact/resources/themes/md-light-indigo/theme.css';
-//import 'primereact/resources/themes/arya-orange/theme.css';
-//import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
-//import 'primereact/resources/themes/lara-dark-blue/theme.css';
 import '@/assets/styles/globals.scss';
 
 import 'primereact/resources/primereact.min.css';
@@ -16,9 +10,6 @@ import 'primeicons/primeicons.css';
 
 // PrimeFlex
 import 'primeflex/primeflex.css';
-
-// Tailwind + your global styles
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
