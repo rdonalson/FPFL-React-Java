ffec007e4f998e5675ffbf95fb285571b4cc7884
Rick Donalson <rdonalson@users.noreply.github.com>
2026-09-07 15:19:57 -0400

    Remove commented-out WebConfig class and CORS configuration

--- API-JDK/module-api/src/main/java/com/financialplanner/moduleapi/config/WebConfig.java
+++ API-JDK/module-api/src/main/java/com/financialplanner/moduleapi/config/WebConfig.java
@@ -1,29 +1,29 @@
-package com.financialplanner.moduleapi.config;
-
-import org.jspecify.annotations.NonNull;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * Configuration class responsible for setting up web-related configurations for the application.
- * This class customizes the behavior of CORS (Cross-Origin Resource Sharing) to allow external
- * access to the API endpoints.
- */
-@Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigure() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE");
-            }
-        };
-    }
-}
+//package com.financialplanner.moduleapi.config;
+//
+//import org.jspecify.annotations.NonNull;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * Configuration class responsible for setting up web-related configurations for the application.
+// * This class customizes the behavior of CORS (Cross-Origin Resource Sharing) to allow external
+// * access to the API endpoints.
+// */
+//@Configuration
+//public class WebConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigure() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(@NonNull CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE");
+//            }
+//        };
+//    }
+//}
 
 
